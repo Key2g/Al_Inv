d471dbc00b576de0add30fe4e2834814083c8ca8
Key2g <key2gletterbox@gmail.com>
2024-07-04 15:15:20 +0200

    zaktualizowano readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Alien Invaders Project from Python Crash course
 
 #first real repo
+
+#current WIP: chap. 14 p. 292
--- game_functions.py
+++ game_functions.py
@@ -68,8 +68,6 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
 def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     """Respond to bullet-alien collisions."""
     # Remove any bullets and aliens that have collided.
-    # Check for any bullets that have hit aliens.
-    # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     
     if len(aliens) == 0:
